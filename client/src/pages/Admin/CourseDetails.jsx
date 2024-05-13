1582aa5eec2e94168d56639e69c3a8e21a7dbfa6
poorna-theekshana <poornatheekshana@outlook.com>
2024-05-13 16:19:17 +0530

    home and course details changes

--- client/src/pages/Admin/CourseDetails.jsx
+++ client/src/pages/Admin/CourseDetails.jsx
@@ -1,48 +1,70 @@
-import React, { useState, useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { courseDetailsById } from '../../redux/actions/userAction';
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { courseDetailsById } from "../../redux/actions/userAction";
+import { Link } from "react-router-dom";
 
 const CourseDetails = (props) => {
-    const dispatch = useDispatch();
-    const singleCourse = useSelector(store => store.courseRoot.singleCourse);
-    const [course, setCourse] = useState({});
+  const dispatch = useDispatch();
+  const singleCourse = useSelector((store) => store.courseRoot.singleCourse);
+  const [course, setCourse] = useState({});
 
-    useEffect(() => {
-        dispatch(courseDetailsById(props.match.params.courseId));
-    }, [props.match.params.courseId]);
+  useEffect(() => {
+    dispatch(courseDetailsById(props.match.params.courseId));
+  }, [dispatch, props.match.params.courseId]);
 
-    useEffect(() => {
-        if (singleCourse) {
-            setCourse(singleCourse);
-        }
-    }, [singleCourse]);
+  useEffect(() => {
+    if (singleCourse) {
+      setCourse(singleCourse);
+    }
+  }, [singleCourse]);
 
-    return (
-        <div className="container mt-5">
-            <div className="row justify-content-center">
-                <div className="col-md-6">
-                    <br /><br />
-                    {course.file && course.file.map((video, index) => (
-                        <div key={index} className="mb-3 d-flex justify-content-center">
-                            <div className="card" style={{ width: '18rem' }}>
-                                {/* <video width="100%" controls>
-                                    <source src={video} type="video/mp4" />
-                                </video> */}
-                               
-                                <div className="card-body text-center">
-                                    <h5 className="card-title">Episode {index + 1}</h5>
-                                    <p className="card-text">{video.title}</p>
-                                    <Link to={`/play-video/${props.match.params.courseId}/${index}`} className="btn btn-primary">Watch Now</Link>
-                                </div>
-                            </div>
-                        </div>
-                    ))}
-                </div>
-            </div>
-            <Link to={`/quizByCourse/${props.match.params.courseId}`} className="btn btn-primary">Quiz</Link>
+  return (
+    <div className="container mt-5 pt-5">
+      <div className="row justify-content-center align-items-center">
+        {/* Course Image */}
+        <div className="col-md-6 text-center">
+          {course.image && (
+            <img src={course.image} alt="Course" className="img-fluid mb-3" />
+          )}
         </div>
-    );
+
+        {/* Course Details and Video List */}
+        <div className="col-md-6">
+          <h3 className="text-center my-4">Course Videos</h3>
+          {course.file &&
+            course.file.map((video, index) => (
+              <div key={index} className="card mb-3">
+                <div className="card-body">
+                  <h5 className="card-title">
+                    Episode {index + 1}: {video.title}
+                  </h5>
+                  <Link
+                    to={`/play-video/${props.match.params.courseId}/${index}`}
+                    className="btn btn-primary"
+                  >
+                    Watch Now
+                  </Link>
+                </div>
+              </div>
+            ))}
+          <h2>{course.title}</h2>
+          <p>{course.description}</p>
+          <p>
+            <strong>Category:</strong> {course.category}
+          </p>
+          <p>
+            <strong>Duration:</strong> {course.duration} minutes
+          </p>
+          <Link
+            to={`/quizByCourse/${props.match.params.courseId}`}
+            className="btn btn-success mt-2"
+          >
+            Start Quiz
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default CourseDetails;
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,49 +1,55 @@
-import React, { useState, useEffect } from 'react'
-import { useSelector, useDispatch } from 'react-redux'
-import { allCourses } from '../redux/actions/userAction'
-import Navbar from '../components/Navbar'
-import Card from '../components/Card'
+import React, { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { allCourses } from "../redux/actions/userAction";
+import Navbar from "../components/Navbar";
+import Card from "../components/Card";
+
 const Home = () => {
-    const dispatch = useDispatch()
-    const store = useSelector(store => store.courseRoot)
-    const [courses, setAllCourses] = useState([])
-    const [isLoading, setIsLoading] = useState(false)
-    useEffect(() => {
-        dispatch(allCourses())
-        setIsLoading(true)
-    }, [])
-    useEffect(() => {
-        if (store.allCourse.length !== 0) {
-            setAllCourses(store.allCourse)
-            setIsLoading(false)
-        }
-        
-    }, [store.allCourse])
-    return (
-        <>
-            <div className="container" style={{ marginTop: "100px" }}>
-                <div className="row">
+  const dispatch = useDispatch();
+  const store = useSelector((store) => store.userRoot); // Assuming userRoot has user data
+  const coursesStore = useSelector((store) => store.courseRoot); // Assuming courseRoot has courses data
+  const [courses, setAllCourses] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
+  useEffect(() => {
+    dispatch(allCourses());
+    setIsLoading(true);
+  }, [dispatch]);
 
-                </div>
-                <div className="row">
-                    <div class="row justify-content-center">
-                        <div class="col-md-1">
-                            {
-                                isLoading && <div class="spinner-border text-primary" role="status">
-                                    <span class="sr-only">Loading...</span>
-                                </div>
-                            }
-                        </div>
-                    </div>
-                    {courses.map((data,index) =>
-                        <Card key={index} course={data} />
-                        )}
-                   
-                </div>
+  useEffect(() => {
+    if (coursesStore.allCourse.length !== 0) {
+      setAllCourses(coursesStore.allCourse);
+      setIsLoading(false);
+    }
+  }, [coursesStore.allCourse]);
+
+  return (
+    <>
+      <Navbar />
+      <div className="container" style={{ marginTop: "100px" }}>
+        {/* Welcome message and app description */}
+        <div className="row">
+          <div className="col-12 text-center">
+            <h1>Welcome, {store.user.name}!</h1>
+            <p>
+              Explore our wide range of courses and expand your knowledge with
+              AcademyNet.
+            </p>
+            <hr />
+          </div>
+        </div>
+        <div className="row justify-content-center">
+          {isLoading ? (
+            <div className="spinner-border text-primary" role="status">
+              <span className="sr-only">Loading...</span>
             </div>
-        </>
-    )
-}
+          ) : (
+            courses.map((data, index) => <Card key={index} course={data} />)
+          )}
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default Home
+export default Home;
